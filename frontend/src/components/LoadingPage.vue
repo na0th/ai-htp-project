2333716284df448fd95caa0e900c0ddbb06379ef
YoungLeigh <yoonyoung.lee1218@gmail.com>
2023-02-22 00:38:33 +0900

    loading page edit

--- frontend/src/components/LoadingPage.vue
+++ frontend/src/components/LoadingPage.vue
@@ -1,12 +1,14 @@
 <template>
   <ResultData v-if="data" v-bind:data="data"></ResultData>
   <div v-else class="loading-page">
+    <img class="paint-spring" src="../assets/images/paintSpring.png" />
     <div class="loading-text">
-      <p>
-        '마음 스케치' 프로젝트는 유저가 직접 그린 그림에 AI 기술을 더하여 본인의
-        심리 및 정서 상태를 파악할 수 있도록 돕고자 제작되었습니다. 본인의
-        상태가 심각하다고 판단되거나, 보다 정확한 해석을 알기 위해서는 전문
-        의료진을 찾아주세요.
+      <p class="loading-maintext">
+        <span style="color: #eeb3ad; font-weight: 1000">'마음 스케치'</span>
+        프로젝트는 AI 기술을 통해 유저가 직접 그린 그림을 분석하여 본인의 심리
+        및 정서 상태를 파악할 수 있도록 돕고자 제작되었습니다. 본인의 상태가
+        심각하다고 판단되거나, 보다 정확한 해석을 알기 위해서는 전문 의료진을
+        찾아주세요.
       </p>
       <p class="member-text">
         프로젝트 참여 인원: <br /><br />
@@ -14,6 +16,7 @@
         Back-End: 양채린, 황혜수, 백소미 <br />
         AI: 이수민, 이겸희 <br />Design: 이가윤
       </p>
+      <img class="loading-img" src="../assets/images/heart.png" />
     </div>
     <p class="touch-text1">잠시만 기다려주세요...</p>
   </div>
@@ -36,25 +39,34 @@ export default {
 .loading-page {
   height: calc(var(--vh, 1vh) * 100);
   background-image: url("../assets/images/night.jpg");
-  font-family: korFont2;
+  overflow: hidden;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  font-family: korFont3;
 }
 .loading-text {
-  line-height: 1.3;
-  width: 85%;
+  width: 80%;
   font-family: korFont2;
   display: flex;
   flex-direction: column;
-  text-align: left;
-  padding-left: 25px;
-  padding-top: 50px;
-  font-size: 25px;
+  padding-top: 70px;
+  display: inline-block;
+  align-items: center;
 }
-.loading-text:first-child {
-  font-size: 20px;
+.loading-maintext {
+  font-size: 18px;
+  text-align: left;
+  line-height: 2;
 }
 .member-text {
-  padding-top: 150px;
+  padding-top: 50px;
   font-size: 20px;
+  font-family: korFont3;
+  line-height: 1.3;
 }
 .touch-text1 {
   color: #dededeb9;
@@ -64,8 +76,19 @@ export default {
   left: 0;
   right: 0;
   text-align: center;
-  bottom: 20%;
+  bottom: 7%;
   font-size: 17px;
   animation: blinker 2s linear infinite;
+}
+.paint-spring {
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.loading-img {
+  width: 45px;
+  display: inline-block;
+  margin-top: 30px;
 }
 </style>
