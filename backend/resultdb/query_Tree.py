e510e7be0a1e2c68753f058db1bbba329480a8e8
baeksom <97323941+baeksom@users.noreply.github.com>
2023-02-22 19:24:31 +0900

    Add files via upload

--- backend/resultdb/query_Tree.py
+++ backend/resultdb/query_Tree.py
@@ -39,23 +39,23 @@ for i in range(0,len(stem_rlabel)):
 # size_rlabel 결과라벨 => [0,0,0,0] [작은그림,그림위치,줄기길이,줄기굵기]
 size_rlabel = [0,0,0,0]
 if size_rlabel[0] == 1: # 작은 그림일때
-    save_result(TreeSize,1)
+    save_result(TreeSize,0)
 
 if size_rlabel[1] == 0: # 그림 위치 오른쪽 0
-    save_result(TreeSize,2)
+    save_result(TreeSize,1)
 elif size_rlabel[1] == 1: # 그림 위치 가운데 1
-    save_result(TreeSize,3)
+    save_result(TreeSize,2)
 elif size_rlabel[1] == 2: # 그림 위치 왼쪽 2
-    save_result(TreeSize,4)
+    save_result(TreeSize,3)
 
 if size_rlabel[2] == 1: # 줄기 길이 길때
-    save_result(TreeSize,5)
+    save_result(TreeSize,4)
 else: # 줄기 길이 짧을때
-    save_result(TreeSize,6)
+    save_result(TreeSize,5)
 
 if size_rlabel[3] == 1: # 줄기 굵기 두꺼울때
-    save_result(TreeSize,7)
+    save_result(TreeSize,6)
 else: # 줄기 굵기 얇을때
-    save_result(TreeSize,8)
+    save_result(TreeSize,7)
 
 
