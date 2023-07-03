c8f2aa4e48da9b40ca03f4ee17c279eac17261db
건 초이 <105775683+na0th@users.noreply.github.com>
2023-07-04 01:27:24 +0900

    favicon,카카오톡 공유기능 url, 링크 클립보드 복사 url,메타데이터 수정, IP 환경 변수로 설정 (#14)
    * favicon,카카오톡 공유기능 url, 링크 클립보드 복사 url,메타데이터 수정
    
    favicon,카카오톡 공유기능 url, 링크 클립보드 복사 url,메타데이터 수정
    
    * IP 환경 변수로 설정
    
    .
    
    * IP 환경 변수로 설정(수정)
    
    .

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    host: process.env.VUE_APP_IP_ADDRESS,
     port: process.env.VUE_APP_FLASK_PORT,
   },
 })
